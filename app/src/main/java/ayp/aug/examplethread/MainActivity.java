98b7c34fb807aa77f8dcad91633845459b62e25b
Hattapong <Hattapong.M@augmentis.biz>
2016-09-14 16:06:04 +0700

    AsyncTask

--- app/src/main/java/ayp/aug/examplethread/MainActivity.java
+++ app/src/main/java/ayp/aug/examplethread/MainActivity.java
@@ -6,26 +6,33 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 import javax.net.ssl.HandshakeCompletedListener;
 
 public class MainActivity extends AppCompatActivity {
 
     public TextView txtView;
+    public Button btnView;
+    public EditText editView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnView = (Button)findViewById(R.id.btn_1);
+        editView = (EditText)findViewById(R.id.edittxt_1);
         txtView = (TextView)findViewById(R.id.text_1);
-        txtView.setOnClickListener(new View.OnClickListener() {
+        btnView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ExampleTask exampleTask = new ExampleTask();
-                exampleTask.execute(1);
+                exampleTask.execute(Integer.parseInt(editView.getText().toString()));
             }
         });
 
@@ -37,19 +44,21 @@ public class MainActivity extends AppCompatActivity {
         protected String doInBackground(Integer... args) {
             int i = args[0]; // i = 1;
             while(true){
-                i++;
+
                 try {
-                    Thread.sleep(1000);
+//                    Thread.sleep(1000);
+                    TimeUnit.SECONDS.sleep(1);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 publishProgress(i); //invoke UI Thread to onProgressUpdate
-                if(i == 100){
+                i--;
+                if(i == 0){
                     break;
                 }
             }
 
-            return "Hello";
+            return "Finish";
         }
 
         @Override
